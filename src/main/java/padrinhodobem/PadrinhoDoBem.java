1a2ea8f68a1f0a9874d40f0122ff68bd87b0303d
Herbert FB <herbert.beserra@gmail.com>
2023-05-30 22:08:20 -0300

    Novo tema do java

--- src/main/java/padrinhodobem/PadrinhoDoBem.java
+++ src/main/java/padrinhodobem/PadrinhoDoBem.java
@@ -4,6 +4,10 @@
 
 package padrinhodobem;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import com.formdev.flatlaf.FlatLightLaf;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import padrinhodobem.view.Login;
@@ -17,18 +21,24 @@ public class PadrinhoDoBem {
     public static void main(String[] args) {
         
         try {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-            
-                System.out.println(info.getName());
-                
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e);
+            UIManager.setLookAndFeel( new FlatDarculaLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
         }
+        
+//        try {
+//            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+//            
+//                System.out.println(info.getName());
+//                
+//                if ("Nimbus".equals(info.getName())) {
+//                    UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
 //        JOptionPane.showConfirmDialog(null, "oi");
         
         Login lg = new Login();
